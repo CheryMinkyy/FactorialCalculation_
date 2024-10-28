cf892d7d8221bad1dbba86906fe5ed758b6860ed
CheryMinkyy <nisanurkosyanli@gmail.com>
2024-10-28 23:40:49 +0300

    Update factorial_recursive.py

--- factorial_recursive.py
+++ factorial_recursive.py
@@ -5,4 +5,6 @@ def factorial_recursive(n):
         return n * factorial_recursive(n - 1)
 
 # Example usage
-print(factorial_recursive(5))  # Output: 120
+if __name__ == "__main__":
+    number = int(input("Enter a non-negative integer: "))  # User input
+    print(factorial_recursive(number))  # Output: Factorial of the entered number
